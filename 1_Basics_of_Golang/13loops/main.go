fb0705a7d44a96ee4a17463b71aa317dd7a44cf1
Tanmay Thanvi <tanmaythanvi15@gmail.com>
2023-11-24 11:33:45 +0530

    Adding Infinite Loop

--- 1_Basics_of_Golang/13loops/main.go
+++ 1_Basics_of_Golang/13loops/main.go
@@ -1,48 +1,62 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func main() {
 	fmt.Println("----Loops----")
 
-	days := []string{"Sunday","Monday","Tuesday","Wednesday"}
+	days := []string{"Sunday", "Monday", "Tuesday", "Wednesday"}
 
 	fmt.Println(days)
 
 	// there is only one loop : for but have many variation
-	for d:=0; d<len(days); d++{ // there is nothing like ++d
+	for d := 0; d < len(days); d++ { // there is nothing like ++d
 		fmt.Println(days[d])
 	}
 
-	for i := range days{
-		fmt.Println(i,days[i]) // i gives index here 
+	for i := range days {
+		fmt.Println(i, days[i]) // i gives index here
 	}
 
-	for index,day := range days{
-		fmt.Println(index,day)
+	for index, day := range days {
+		fmt.Println(index, day)
 	}
 
 	roguevalue := 1
 
-	for roguevalue < 10{
-		
+	for roguevalue < 10 {
+
 		if roguevalue == 7 {
 			break
 		}
 
-		if roguevalue == 5{
+		if roguevalue == 5 {
 			roguevalue++
 			continue
 		}
-		
+
 		if roguevalue == 2 {
 			goto lco
 		}
 
-		fmt.Println("Value : ",roguevalue)
-		roguevalue++;
+		fmt.Println("Value : ", roguevalue)
+		roguevalue++
 	}
 
-	lco: 
-		fmt.Println("Jumping at lco.dev")
+lco:
+	fmt.Println("Jumping at lco.dev")
+
+	// infinite loop in golang (Asked in Pubmatic)
+	cnt := 0
+	for {
+		// if i > 50 {
+		// 	break;
+		// }
+		cnt += 5
+		fmt.Println("i = ", cnt)
+		time.Sleep(5)
+	}
 }
